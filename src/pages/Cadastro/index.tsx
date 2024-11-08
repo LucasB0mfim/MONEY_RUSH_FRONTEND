b1ad8e50a6c16fd24762d10cdc36b7279f4d0fcc
lucas <lucasbomfimsobral@gmail.com>
2024-11-08 11:11:21 -0300

    Feat: adicionar validação de campos no formulário de cadastro

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+import * as yup from 'yup'
+import { useFormik } from 'formik'
 
 import Loader from '../../components/Loader'
 
@@ -10,11 +12,27 @@ import * as S from './styles'
 const Cadastro = () => {
 
   const [register, setRegister] = useState(true)
-
   const navigate = useNavigate()
-  if (register !== true) {
-    navigate('/access-account')
-  }
+
+  const form = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      username: '',
+      password: ''
+    },
+    validationSchema: yup.object({
+      name: yup.string().min(3, 'O nome precisa ter pelo menos 3 caracteres').required('O campo é obrigatório'),
+      email: yup.string().required('O campo é obrigatório'),
+      username: yup.string().min(3, 'O nome precisa ter pelo menos 3 caracteres').required('O campo é obrigatório'),
+      password: yup.string().min(3, 'O nome precisa ter pelo menos 8 caracteres').required('O campo é obrigatório'),
+    }),
+    onSubmit: (values) => {
+      console.log(values)
+      setRegister(false)
+      navigate('/access-account')
+    }
+  })
 
   return (
     <S.Container>
@@ -25,15 +43,24 @@ const Cadastro = () => {
         </S.DivLogo>
         <h2>O JEITO MAIS INTELIGENTE DE CONTROLAR OS GASTOS</h2>
       </S.Aside>
-      <S.Main>
+      <S.Main isError={Object.keys(form.errors).length > 0}>
         <h2>Cadastro</h2>
         <p>Preencha os campos abaixo para se cadastrar</p>
-        <S.Form>
-          <S.Input type="text" placeholder='Digite seu nome completo'/>
-          <S.Input type="text" placeholder='Digite seu e-mail'/>
-          <S.Input type="text" placeholder='Digite seu usuario'/>
-          <S.Input type="text" placeholder='Digite sua senha' />
-          <S.BtnEnter type="button" onClick={ () => setRegister(false)} >{register ? 'Cadastrar' : <Loader />}</S.BtnEnter>
+        <S.Form onSubmit={form.handleSubmit} isError={Object.keys(form.errors).length > 0}>
+
+          {form.touched.name && form.errors.name && <S.Error>{form.errors.name}</S.Error>}
+          <S.Input type="text" placeholder='Digite seu nome completo' id='name' name='name' value={form.values.name} onChange={form.handleChange} onBlur={form.handleBlur} isError={!!(form.touched.name && form.errors.name)} />
+
+          {form.touched.email && form.errors.email && <S.Error>{form.errors.email}</S.Error>}
+          <S.Input type="text" placeholder='Digite seu e-mail' id='email' name='email' value={form.values.email} onChange={form.handleChange} onBlur={form.handleBlur} isError={!!(form.touched.email && form.errors.email)} />
+
+          {form.touched.username && form.errors.username && <S.Error>{form.errors.username}</S.Error>}
+          <S.Input type="text" placeholder='Digite seu usuario' id='username' name='username' value={form.values.username} onChange={form.handleChange} onBlur={form.handleBlur} isError={!!(form.touched.username && form.errors.username)} />
+
+          {form.touched.password && form.errors.password && <S.Error>{form.errors.password}</S.Error>}
+          <S.Input type="password" placeholder='Digite sua senha' id='password' name='password' value={form.values.password} onChange={form.handleChange} onBlur={form.handleBlur} isError={!!(form.touched.password && form.errors.password)} />
+
+          <S.Btn type="submit" isError={Object.keys(form.errors).length > 0} >{register ? 'Cadastrar' : <Loader />}</S.Btn>
         </S.Form>
       </S.Main>
     </S.Container>
--- src/pages/Cadastro/styles.ts
+++ src/pages/Cadastro/styles.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
+
 import { colors } from "../../styles";
+
+type ErrorProps = {
+  isError: boolean
+}
 
 export const Container = styled.div`
   height: 100vh;
@@ -11,29 +16,6 @@ export const Container = styled.div`
   background-repeat: no-repeat;
   background-attachment: fixed;
   background: linear-gradient(140deg, ${colors.greenDark} 10%, ${colors.green} 30%, ${colors.greenLight} 60%);
-`
-
-export const Main = styled.main`
-  width: 30%;
-  height: 100%;
-  padding: 20vh 3vh 30vh 3vh;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
-  background: rgba(255, 255, 255, 0.1);
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
-
-  > h2 {
-    color: ${colors.white};
-    font-size: 3.75vh;
-  }
-
-  > p {
-    color: ${colors.grayLight};
-    font-size: 2.5vh;
-  }
 `
 
 export const Aside = styled.aside`
@@ -66,54 +48,74 @@ export const DivLogo = styled.div`
   }
 `
 
-export const Form = styled.form`
+export const Main = styled.main<ErrorProps>`
+  width: 30%;
+  height: 100%;
+  padding: ;
+  padding: ${(props) => props.isError ? '8% 3vh 8% 3vh' : '20vh 3vh 30vh 3vh'};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: ${(props) => props.isError ? 'center' : 'space-between'};
+
+  background: rgba(255, 255, 255, 0.1);
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+
+  > h2 {
+    color: ${colors.white};
+    font-size: 3.75vh;
+  }
+
+  > p {
+    color: ${colors.grayLight};
+    font-size: 2.5vh;
+    margin-top: ${(props) => props.isError ? '9%' : 'none'};
+  }
+`
+
+export const Form = styled.form<ErrorProps>`
   display: flex;
   align-items: center;
   flex-direction: column;
   justify-content: center;
+
+  margin-top: ${(props) => props.isError ? '9%' : 'none'};
 `
 
-export const Input = styled.input`
+export const Input = styled.input<ErrorProps>`
   color: ${colors.white};
   width: 100%;
   padding: 3.2%;
   font-size: 2.1vh;
 
-  border: none;
   outline: none;
   margin-bottom: 1.08%;
   border-radius: 0.7vh;
-  background: ${colors.greenDark};
+  border: ${(props) => props.isError ? '0.4vh solid rgb(255, 0, 0)' : 'none'};
+  background: ${(props) => props.isError ? 'rgb(139, 0, 0)' : 'rgba(34, 48, 11, 1)'};
 
   &::placeholder {
     color: rgba(255, 255, 255, 0.3);
   }
 `
 
-export const BtnEnter = styled.button`
-  color: ${colors.white};
-  width: 100%;
-  padding: 3.2%;
-  cursor: pointer;
-  font-size: 2.1vh;
+export const Btn = styled.button<ErrorProps>`
+color: ${colors.white};
+width: 100%;
+padding: 3.2%;
+cursor: pointer;
+font-size: 2.1vh;
 
-  border: none;
-  outline: none;
-  margin-bottom: 1.08%;
-  border-radius: 0.7vh;
-  background: ${colors.green};
+border: none;
+outline: none;
+margin-top: ${(props) => props.isError ? '1.08%' : 'none'};
+border-radius: 0.7vh;
+background: ${colors.green};
 `
 
-export const BtnCreate = styled.button`
-color: ${colors.white};
-  width: 100%;
-  padding: 3.2%;
-  cursor: pointer;
-  font-size: 2.1vh;
-
-  outline: none;
-  border: none;
-  border-radius: 0.7vh;
-  background: transparent;
-  border: solid 0.3vh ${colors.gray};
+export const Error = styled.div`
+width: 100%;
+font-size: 2.2vh;
+margin-bottom: 1.08%;
+color: rgb(139, 0, 0);
 `
