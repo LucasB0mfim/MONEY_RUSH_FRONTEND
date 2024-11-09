0c4aa68f416b45d0f954d017485a264a2fbf54ea
lucas <lucasbomfimsobral@gmail.com>
2024-11-09 17:05:53 -0300

    Fix: Corrigir problema do botão no home.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,15 +2,25 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../store';  // Importando o tipo RootState
 
 import * as S from './styles'
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate()
+  const [back, setBack] = useState(true)
   const usuario = useSelector((state: RootState) => state.usuario);  // Acessando os dados do usuário
   const nomeDoUsuario = usuario?.username;  // Pegando o nome do usuário
+
+  useEffect( () => {
+    if (!back) {
+      navigate('/access-account')
+    }
+  })
 
   return (
     <S.Container>
       <h1>{nomeDoUsuario}</h1>
-      <a href="access-account"><S.Btn>Sair</S.Btn></a>
+      <a href="access-account" onClick={() => setBack(false)}><S.Btn>Sair</S.Btn></a>
     </S.Container>
   )
 }
